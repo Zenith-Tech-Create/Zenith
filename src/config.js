9dd21d1b07c3676070b71f79271b42a42efe57a4
Mason Se <localaiworkstation@email.com>
2026-05-09 16:12:11 -0700

    chore: remove debug logging from config

--- src/config.js
+++ src/config.js
@@ -1,14 +1,6 @@
 const path = require('path');
-const fs = require('fs');
-const os = require('os');
 
 const API_KEY = 'LEMONSQUEEZY_KEY_PLACEHOLDER';
-
-// Write debug log to home directory
-const logPath = os.homedir() + '/zenith-debug.log';
-fs.writeFileSync(logPath, '=== ZENITH CONFIG ===\n', { flag: 'a' });
-fs.writeFileSync(logPath, 'API Key status: ' + (API_KEY !== 'LEMONSQUEEZY_KEY_PLACEHOLDER' ? 'SET' : 'PLACEHOLDER NOT REPLACED') + '\n', { flag: 'a' });
-fs.writeFileSync(logPath, 'API Key first 4 chars: ' + API_KEY.substring(0, 4) + '\n', { flag: 'a' });
 
 module.exports = {
   lemonsqueezy: {
